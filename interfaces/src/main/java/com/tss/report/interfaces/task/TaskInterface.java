55e6b0dc7b2ca3656a5bdd55ed571220cf1fba9c
MQG <mo1056610188@163.com>
2018-11-25 23:13:17 +0800

    创建实验任务

--- interfaces/src/main/java/com/tss/report/interfaces/task/TaskInterface.java
+++ interfaces/src/main/java/com/tss/report/interfaces/task/TaskInterface.java
@@ -1,8 +1,12 @@
 package com.tss.report.interfaces.task;
 
+import com.tss.report.interfaces.task.vo.TaskCreateReqVO;
 import com.tss.report.interfaces.task.vo.TaskDetailReqVO;
 import com.tss.report.interfaces.task.vo.TaskDetailRespVO;
 
 public interface TaskInterface {
+
     TaskDetailRespVO getTaskDetail(TaskDetailReqVO param);
+
+    Long createTask(TaskCreateReqVO param);
 }
--- services/src/main/java/com/tss/report/services/task/TaskService.java
+++ services/src/main/java/com/tss/report/services/task/TaskService.java
@@ -1,8 +1,10 @@
 package com.tss.report.services.task;
 
+import com.tss.basic.common.util.JsonUtil;
 import com.tss.basic.common.util.ModelMapperUtil;
 import com.tss.basic.site.exception.DataCheckException;
 import com.tss.report.interfaces.task.TaskInterface;
+import com.tss.report.interfaces.task.vo.TaskCreateReqVO;
 import com.tss.report.interfaces.task.vo.TaskDetailReqVO;
 import com.tss.report.interfaces.task.vo.TaskDetailRespVO;
 import com.tss.report.interfaces.task.vo.TeacherCourseCurriculumProjectRespVO;
@@ -17,7 +19,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -56,5 +61,43 @@ public class TaskService implements TaskInterface {
             detail.setCourseProjectList(teacherCourseProjectInfo.getCourseProjectList());
         }
         return detail;
+    }
+
+    @Override
+    public Long createTask(TaskCreateReqVO param) {
+        // 实验任务
+        Task task = this.buildTask(param);
+        int count = taskDao.insert(task);
+        if (count == 0 || task.getId() == null) {
+            LOG.error("save task error, param={}", JsonUtil.obj2json(param));
+            throw new DataCheckException("保存实验任务失败");
+        }
+        // 实验班级
+        this.saveTaskClass(param, task.getId());
+        return task.getId();
+    }
+
+    private Task buildTask(TaskCreateReqVO param) {
+        Task task = ModelMapperUtil.strictMap(param, Task.class);
+        task.setCreateTime(new Date());
+        task.setUpdateTime(new Date());
+        return task;
+    }
+
+    private void saveTaskClass(TaskCreateReqVO param, Long taskId) {
+        param.getClassList().stream().forEach(e -> {
+            TaskClass taskClass = new TaskClass();
+            taskClass.setTaskId(taskId);
+            taskClass.setTaskName(param.getName());
+            taskClass.setClassId(e.getClassId());
+            taskClass.setClassName(e.getClassName());
+            taskClass.setCreateTime(new Date());
+            taskClass.setUpdateTime(new Date());
+            int effect = taskClassDao.insert(taskClass);
+            if (effect == 0) {
+                LOG.error("save task class error, param={}", JsonUtil.obj2json(param));
+                throw new DataCheckException("保存实验任务班级失败");
+            }
+        });
     }
 }
